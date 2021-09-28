aacd567713f13398a53576798d7ba7a8ba9a6f13
pacodan <jhonpaco@frba.utn.edu.ar>
2021-09-27 22:47:03 -0300

    #2

--- src/ejemplo/Ejemplo1.java
+++ src/ejemplo/Ejemplo1.java
@@ -1,24 +1,32 @@
 package ejemplo;
 
 public class Ejemplo1 {
-	public static double T,TPLL,TPV;
+	public static double T,TPLL,TPV,TF;
 	public static  void condicionesIniciales() {
 		T=0;
-		T=0;
+		TPLL=IA();
 		TPV=Double.MAX_VALUE;
+		TF=60000000;
+	}
+	public static double IA() {
+		return 10;// aca va la fpd
+	}
+	private static void eventoVisitaMedico() {
+		T=TPLL;
+		TPLL=T+IA();
+	}
+	public static void eventoLlegadaDePaciente() {
+		
 	}
 	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		System.out.println("Holas");
-//		Simulación simu= new Simulación();
-//		
-//		simu.condicionesIniciales();
-//		
-//		if(simu.getTPLL()<simu.getTPV()) {
-//			simu.setT()
-//		}
-		condicionesIniciales();
-		System.out.print(TPV);
+		while(T<TF) {
+			condicionesIniciales();
+			System.out.println(TPV);
+			TPV=T;
+			T=10;
+			System.out.println(TPV+";"+T);
+			if(TPLL<TPV)eventoLlegadaDePaciente();
+			else eventoVisitaMedico();
+		}
 	}
-
 }
