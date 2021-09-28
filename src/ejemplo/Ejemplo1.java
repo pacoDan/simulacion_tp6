eba33724c037422f0072ba35ea54eb76c54dbb82
Dan <Dan@asus>
2021-09-27 22:06:44 -0300

    #1

---
+++ src/ejemplo/Ejemplo1.java
@@ -0,0 +1,24 @@
+package ejemplo;
+
+public class Ejemplo1 {
+	public static double T,TPLL,TPV;
+	public static  void condicionesIniciales() {
+		T=0;
+		T=0;
+		TPV=Double.MAX_VALUE;
+	}
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Holas");
+//		Simulación simu= new Simulación();
+//		
+//		simu.condicionesIniciales();
+//		
+//		if(simu.getTPLL()<simu.getTPV()) {
+//			simu.setT()
+//		}
+		condicionesIniciales();
+		System.out.print(TPV);
+	}
+
+}
---
+++ src/ejemplo/Simulación.java
@@ -0,0 +1,24 @@
+package ejemplo;
+
+public class Simulación {
+	public double TPLL,TPV,T; 
+	public Simulación() {
+		// TODO Auto-generated constructor stub
+	}
+	public void condicionesIniciales(){
+		
+	}
+	public double getTPLL() {
+		return TPLL;
+	}
+	public void setTPLL(double tPLL) {
+		TPLL = tPLL;
+	}
+	public double getTPV() {
+		return TPV;
+	}
+	public void setTPV(double tPV) {
+		TPV = tPV;
+	}
+	
+}
